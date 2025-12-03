3e76d7d676ba3313d2243dbd98aa04ecafaee0ac
VICKYHIRASKAR001 <vickyhiraskar@gmail.com>
2025-12-03 16:29:11 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -e
+docker stop simple-python-flask-app || true
+docker rm simple-python-flask-app || true
 
-docker pull vicky1306/simple-python-flask-app
+docker pull vicky1306/simple-python-flask-app:latest
 
-# Run with fixed container name so it's easier to manage
-docker run -d --name flaskapp -p 5000:5000 vicky1306/simple-python-flask-app
+docker run -d \
+    --name simple-python-flask-app \
+    -p 5000:5000 \
+    vicky1306/simple-python-flask-app:latest
