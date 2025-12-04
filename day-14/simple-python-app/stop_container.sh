667709b73d4f7d1d31937637e707561c1b9ec9cc
VICKYHIRASKAR001 <vickyhiraskar@gmail.com>
2025-12-04 13:34:57 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop any running container using port 5000
-CONTAINER_ID=$(docker ps -q --filter "publish=5000")
-
-if [ -n "$CONTAINER_ID" ]; then
-  echo "Stopping container on port 5000: $CONTAINER_ID"
-  docker stop $CONTAINER_ID
-  docker rm $CONTAINER_ID
-else
-  echo "No running container found on port 5000"
-fi
-
+# Stop the running container (if any)
+docker ps | awk -F " " '{print $1}'
 
