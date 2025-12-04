b4aae54228dbac40f89572cef908bc301b80fb14
VICKYHIRASKAR001 <vickyhiraskar@gmail.com>
2025-12-04 13:17:01 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-# Stop old container if exists
-docker stop simple-python-flask-app || true
-docker rm simple-python-flask-app || true
+# Pull the Docker image from Docker Hub
+docker pull vicky1306/simple-python-flask-app
 
-# Pull latest image
-docker pull vicky1306/simple-python-flask-app:latest
-
-# Start container
-docker run -d --name simple-python-flask-app -p 5000:5000 vicky1306/simple-python-flask-app:latest
+# Run the Docker image as a container
+docker run -d -p 5000:5000 vicky1306/simple-python-flask-app 
