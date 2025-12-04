96fa6be1e3ec60a4fc44dfbc5c87fd80584cbe5c
VICKYHIRASKAR001 <vickyhiraskar@gmail.com>
2025-12-04 14:04:58 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid=`docker ps | awk -F " " '{print $1}'`
-docker rm-f $containerid
+# Get running container ID
+containerid=$(docker ps -q)
 
+if [ -n "$containerid" ]; then
+    echo "Stopping container: $containerid"
+    docker stop $containerid
+    echo "Removing container..."
+    docker rm -f $containerid
+else
+    echo "No running container found."
+fi
